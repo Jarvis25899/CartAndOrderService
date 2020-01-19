45fe66f45597e244431d49762675dbcf6ec089bb
Jarvis25899 <35518754+Jarvis25899@users.noreply.github.com>
2020-01-19 18:20:05 +0530

    first commit

---
+++ src/main/java/com/example/CartAndOrderService/CartAndOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.CartAndOrderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartAndOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartAndOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CartAndOrderService/CartAndOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CartAndOrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartAndOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
